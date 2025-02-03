d0c986a5838f13d7393a6dff3adf9ef6fb81cfb8
Dhriti <dhriti.baruah20@gmail.com>
2025-02-03 21:41:34 +0530

    Syncing

--- index.js
+++ index.js
@@ -33,6 +33,60 @@ app.post("/addNote", async (req, res) => {
     res.json(allNotes)
 })
 
+//READ ENDPOINT
+app.get("/notes", async (req, res) => {
+  try {
+    const allNotes = await Note.find({})
+    res.json(allNotes)
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+//FETCH ONE NOTE
+app.get("/oneNote/:id", async (req, res) => {
+  try {
+    const oneNote = await Note.findById({_id: req.params.id})
+    res.json(oneNote)
+  } catch (error) {
+    console.error(error)
+  }
+})
+
+//Update the note
+app.put("/update/:id", async (req, res) => {
+  const {id} = req.params;
+  const title = req.body.title;
+  const note = req.body.note;
+
+  try {
+    if(!title || !note){
+      return res.status(400).json({error: "Title and note are required"})
+    }
+
+    const updatedNote = await Note.findByIdAndUpdate( id, {title, note})
+    if(!updatedNote){
+      return res.status(400).json({ error: "Note not found"})
+    }
+    res.status(200).json({message: "Note updated"})
+  } catch (error) {
+    console.error(error)
+  }
+})
+
+//DELETE OPERATION
+app.delete("/delete/:id", async (req, res) => {
+  try {
+    const deleteNote = await Note.findByIdAndDelete({ _id: req.params.id})
+    if(!deleteNote){
+      return res.status(400).json({ error: "Note not found"})
+    }
+    res.status(200).json({message: "Note deleted"})
+  } catch (error) {
+    console.error(error)
+  }
+})
+
 app.listen(PORT, () => {
   console.log(`Server started at port ${PORT}`);
 });
